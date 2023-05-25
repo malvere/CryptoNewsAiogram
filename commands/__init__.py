667952cde7868ae89a717fd29f223adb51e4ecc1
Malvere <gatling1404@gmail.com>
2023-05-25 15:17:38 +0300

    added admin permissions

--- commands/__init__.py
+++ commands/__init__.py
@@ -8,5 +8,5 @@ from .tools.admin_handler import admin, admin_only
 
 def setup(dp: Dispatcher) -> None:
     dp.middleware.setup(ProcessorMiddleware())
-    # dp.register_message_handler(admin, admin_only, commands=["start"])
+    dp.register_message_handler(admin, admin_only, commands=["start"])
     dp.register_message_handler(start, commands=["start"])
